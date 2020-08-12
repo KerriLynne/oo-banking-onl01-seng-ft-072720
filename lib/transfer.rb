966f0b4fe3ae59f1c5fa18ae2d08b7fd70954ac7
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-12 22:33:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,36 +22,12 @@ class Transfer
         @status = "complete"
     end
   end
-      # BankAccount.all.map do | person |
-      #   if person == @sender
-      #     person.balance -= @amount
-      #   elsif person == @receiver
-      #     person.balance += @amount
-      #   end
-      # end
-  #   #
-  #   # end
-  #   # @status = "complete"
-  #
-  # end
+
+  def reverse_transfer
 
 
 
 
-
-  # def execute_transaction
-  #   if !self.valid? && @sender.balance < @amount
-  #     @status = "rejected"
-  #       return "Transaction rejected. Please check your account balance."
-  #
-  #   elsif @status == "complete"
-  #     puts "Transaction was already executed"
-  #   else
-  #     @sender.deposit( @amount * -1 )
-  #     @receiver.deposit( @amount )
-  #     @status = "complete"
-  #   end
-  # end
 
 
 end
